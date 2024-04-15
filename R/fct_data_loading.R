a9ac95687af5e100edd4fe3c0433e3e4370cf6a2
Fox Sebastian (BNSSG CCG) <Sebastian.Fox@BNSSGCCG.NHS.UK>
2024-04-15 10:29:00 +0100

    includes broad age bands in the data loading step

--- R/fct_data_loading.R
+++ R/fct_data_loading.R
@@ -46,11 +46,13 @@ performance_metrics <- function() {
 #'   and metric
 #' @param ics_code string; health code for ics of interest
 #' @param domain_type combination of "Performance", "Demand" or "Capacity"
+#' @param broad_age_bands logical; whether to use broad age bands (TRUE) or not
+#'   (FALSE)
 #' @importFrom purrr map_df
-#' @importFrom dplyr distinct pull filter arrange inner_join join_by
+#' @importFrom dplyr distinct pull filter arrange inner_join join_by mutate
 #' @importFrom rlang sym
 #' @noRd
-ics_data <- function(ics_code, domain_type = NULL) {
+ics_data <- function(ics_code, domain_type = NULL, broad_age_bands = TRUE) {
 
   if (!is.null(domain_type)) {
     if(!any(domain_type %in% c("Performance", "Demand", "Capacity"))) {
@@ -84,7 +86,14 @@ ics_data <- function(ics_code, domain_type = NULL) {
     filter(
       grepl("annual", !!sym("frequency")),
       !!sym("org") == ics_code
-    ) |>
+    )
+
+  if (broad_age_bands == TRUE) {
+    ics_timeseries <- replace_narrow_age_bands(ics_timeseries)
+
+  }
+
+  ics_timeseries <- ics_timeseries |>
     inner_join(
       metrics,
       by = join_by(!!sym("metric"))
@@ -102,6 +111,65 @@ ics_data <- function(ics_code, domain_type = NULL) {
       "numerator",
       "denominator",
       "value"
+    ) |>
+    mutate(
+      value_type = "observed"
+    )
+
+  return(ics_timeseries)
+}
+
+
+#' Replace narrow age bands with broad age bands within tibble
+#'
+#' @param ics_timeseries tibble of data for ics, by year and metric
+#'
+#' @return tibble with same structure as input data but where the metrics that
+#'   refer to age bands having been converted from narrow (10 year) age bands to
+#'   broad
+#' @importFrom dplyr filter mutate summarise any_of mutate bind_rows
+#' @importFrom stringr str_replace_all
+#'
+replace_narrow_age_bands <- function(ics_timeseries) {
+  age_band_data <- ics_timeseries |>
+    filter(
+      grepl("age band", metric)
+    ) |>
+    mutate(
+      metric = str_replace_all(
+        metric,
+        c("0-9" = "0-29",
+          "10-19" = "0-29",
+          "20-29" = "0-29",
+          "30-39" = "30-59",
+          "40-49" = "30-59",
+          "50-59" = "30-59",
+          "60-69" = "60+",
+          "70-79" = "60+",
+          "80-89" = "60+",
+          "80\\+" = "60+",
+          "90\\+" = "60+")
+      )
+    ) |>
+    summarise(
+      numerator = sum(numerator),
+      denominator = mean(denominator),
+      .by = any_of(
+        c(
+          "year", "quarter", "month", "org", "frequency", "metric"
+        )
+      )
+    ) |>
+    mutate(
+      value = numerator / denominator
+    )
+
+  ics_timeseries <- ics_timeseries |>
+    filter(
+      !grepl("age band", metric)
+    ) |>
+    bind_rows(
+      age_band_data
     )
 
   return(ics_timeseries)
